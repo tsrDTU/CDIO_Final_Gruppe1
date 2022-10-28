d69c5b88934ac0f4975df5a45f1d7591eae929d7
UffeBC <113894934+UffeBC@users.noreply.github.com>
2022-10-28 17:49:27 +0200

    Commit 2 copy paste

---
+++ src/main/java/Die.java
@@ -0,0 +1,42 @@
+public class Die
+{
+    private int faceValue = 0, numberOfSides = 6;
+
+
+
+    public Die()
+    {
+        faceValue = (int)(Math.random() * numberOfSides) + 1;
+
+    }
+
+    public Die(int upperBound)
+    {
+        faceValue = (int)(Math.random() * upperBound) + 1;
+
+    }
+    public void Die_test (int lowerBound, int upperBound)
+    {
+        int temp =(int)(Math.random() * upperBound-lowerBound) + 1;
+        faceValue = temp+lowerBound;
+        //       System.out.println(temp + lowerBound);
+        //       faceValue = upperBound;
+    }
+
+    public void dice_roll ()
+    {
+        faceValue = (int)(Math.random() * numberOfSides) + 1;
+    }
+
+
+    public int getFaceValue(){
+        return faceValue;
+    }
+
+
+    public void setNumberOfSides(int newNumberOfSides)
+    {
+        numberOfSides=newNumberOfSides;
+    }
+
+}
