ac7d510f545ac0206877a5aed4002d5db20ac61d
tsrDTU <113907153+tsrDTU@users.noreply.github.com>
2022-11-03 16:39:57 +0100

    Opdater master

--- src/main/java/Chance.java
+++ src/main/java/Chance.java
@@ -1,12 +1,10 @@
-public class Chance {
-    private static ChanceCard[] chanceCards=new ChanceCard[24];
+public  class Chance {
+    public static ChanceCardLoc[] chanceCards=new ChanceCardLoc[24];
 
 
-    chanceCards[0] =new void ChanceCard[0]=
+    chanceCards[0] = new  ChanceCardLoc ("Bilen og tag et chancekort mere","Bil: På din næste tur skal du drøne frem til hvilket som helst ledige felt og købe det. hvis der ikke er nogen ledige felter skal du købe et fra en anden spiller",1);
 
-    ("Bilen og tag et chancekort mere","Bil: På din næste tur skal du drøne frem til hvilket som helst ledige felt og købe det. hvis der ikke er nogen ledige felter skal du købe et fra en anden spiller",1) {
-
-    }
+  // chanceCards[0]="asFEESG";
 
 }
 
--- src/main/java/ChanceCard.java
+++ src/main/java/ChanceCard.java
@@ -4,13 +4,13 @@
  * info: The information body of the card
  * action: The action the card initiates. 0= No action, 1 move to the first free field, 3 move jail.
  */
-public abstract class ChanceCard implements Comparable<ChanceCard> {
+public  class ChanceCardLoc implements Comparable<ChanceCardLoc> {
 
     private String name;
     private String info;
     private int action;
 
-    public ChanceCard ( String nameIn,String infoIn,int actionIn)
+    public ChanceCardLoc ( String nameIn,String infoIn,int actionIn)
     {
         name=nameIn;
         info=infoIn;
@@ -23,4 +23,8 @@ public abstract class ChanceCard implements Comparable<ChanceCard> {
     }
 
 
+    @Override
+    public int compareTo(ChanceCardLoc o) {
+        return 0;
+    }
 }
