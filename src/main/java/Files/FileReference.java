5ac9802111a9a4468ab92b00689b6d0fd6819a7d
UffeBC <113894934+UffeBC@users.noreply.github.com>
2022-11-18 10:22:30 +0100

    Lilla til brun farve ændring

--- src/main/java/Files/FileReference.java
+++ src/main/java/Files/FileReference.java
@@ -16,42 +16,16 @@ public class FileReference {
 //------------------------------------------------------------------------------------------
 //public static String TitleF = new File("src/main/Field Guts/Title");
     //public static File DescriptionF = new File("src/main/Field Guts/description");
-    public static final String DescriptionSt = new File("src/main/Field Guts/description").getAbsolutePath();
-    public static File DescriptionF(){
-        try {
-        return new File(DescriptionSt);
+    public static File DescriptionF = new File("src/main/Field Guts/description");
+    public static File TitleF = new File("src/main/Field Guts/Title");
 
-        }catch (Exception e) {
-            System.out.println("error");
-        }
-        return null;
-    }
+    public static File subtextF = new File("src/main/Field Guts/subText");
 
-
-
-    public static final String TitleSt = new File("src/main/Field Guts/Title").getPath();
-    public static File TitleF = new File(TitleSt);
-
-
-
-    //private static final InputStream subtextI = this.getClass().getResourceAsStream("src/main/Field Guts/subtext");
-    //public static InputStream subtextF = FileReference.class.getResourceAsStream("src/main/Field Guts/subText");
-    public static final String subtextSt = new File("src/main/Field Guts/subText").getPath();
-    public static File subtextF = new File(subtextSt);
-
-    public static final String rentSt = new File("src/main/Field Guts/rent").getPath();
-    public static File rentF = new File(rentSt);
+    public static File rentF = new File("src/main/Field Guts/rent");
 
     public static final String CostToOwnFieldSt = new File("src/main/Field Guts/CostToOwnField").getPath();
     public static File CostToOwnFieldF = new File(CostToOwnFieldSt);
 
-
-    /*
-    public static File CostToOwnFieldFP() {
-        CostToOwnFieldF = new File(String.valueOf(CostToOwnFieldF.getParentFile().mkdirs()));
-        return CostToOwnFieldF;
-    }
-    */
 
 //------------------------------------------------------------------------------------------
 //      Languages for
