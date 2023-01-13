b18b77cbd3c4f86386adb69e11b5daaedc1107bd
tsrDTU <113907153+tsrDTU@users.noreply.github.com>
2023-01-13 08:48:10 +0100

    Test situation flere slag

---
+++ src/main/java/Tests/Test_Faengsel_Amnisti.java
@@ -0,0 +1,20 @@
+package Tests;
+
+import java.io.IOException;
+
+
+public class Test_Faengsel_Amnisti {
+    public static void main(String[] args) throws IOException {
+        {
+            int[] pos_pl1= new int[5];
+            int[] pos_pl2= new int[5];
+            int[] slag= new int[5];
+            int[] chancekort_nr= new int[5];
+
+            pos_pl1[0]=0;
+
+ //           TestSituation tesBetalBanken = new TestSituation(0,0,2,17);
+
+        }
+    }
+}
